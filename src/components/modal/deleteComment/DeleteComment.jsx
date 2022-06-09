186b37ff707d42ed8513fda425f98fd87c1615bb
Peter Carl Pardo <peterpardo123@gmail.com>
2022-06-09 18:45:11 +0800

    Fixed title and buttons

--- src/components/modal/deleteComment/DeleteComment.jsx
+++ src/components/modal/deleteComment/DeleteComment.jsx
@@ -1,19 +1,34 @@
 import "./deleteComment.css";
-import React from 'react';
+import React from "react";
 
 const DeleteComment = ({ openDeleteModal, setOpenDeleteModal }) => {
   return (
-    <div className="deleteModalContainer">
-      <div className="dmWrapper">
-        <h1 className="dmTitle">Delete Comment</h1>
-        <p className="dmDesc">Are you sure you want to delete this comment? This will remove the comment and can't be undone.</p>
-        <div className="dmBtnContainer">
-          <button className="cancelBtn" onClick={() => setOpenDeleteModal(!openDeleteModal)}>NO, CANCEL</button>
-          <button className="confirmBtn" onClick={() => setOpenDeleteModal(!openDeleteModal)}>YES, DELETE</button>
+    <div className='deleteModalContainer'>
+      <div className='dmWrapper'>
+        <h1 className='dmTitle'>Delete Comment</h1>
+        <p className='dmDesc'>
+          Are you sure you want to delete this comment? This will remove the
+          comment and can't be undone.
+        </p>
+        <div className='dmBtnContainer'>
+          <button
+            type='button'
+            className='cancelBtn'
+            onClick={() => setOpenDeleteModal(!openDeleteModal)}
+          >
+            NO, CANCEL
+          </button>
+          <button
+            type='button'
+            className='confirmBtn'
+            onClick={() => setOpenDeleteModal(!openDeleteModal)}
+          >
+            YES, DELETE
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DeleteComment
+export default DeleteComment;
